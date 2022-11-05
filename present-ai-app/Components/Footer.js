00f0c2d13458228844ad5fac64c725da2cfa6875
Omer Kandemir <farukkandemir585@gmail.com>
2022-11-05 19:06:42 -0400

    main page

--- present-ai-app/Components/Footer.js
+++ present-ai-app/Components/Footer.js
@@ -1,10 +1,127 @@
 import React from 'react'
 import styled from 'styled-components'
+import Image from 'next/image';
+import InstagramIcon from '../assets/InstagramIcon.png'
+import FacebookIcon from '../assets/FacebookIcon.png'
+import YouTubeIcon from '../assets/YouTubeIcon.png'
+import TwitterIcon from '../assets/TwitterIcon.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <MainBody>
+      <MainContainer>
+        <ThirdContainer>
+          <SocialMediaGrid>
+            <SocialMediaIcon><Image src={InstagramIcon}/></SocialMediaIcon>
+            <SocialMediaIcon><Image src={FacebookIcon}/></SocialMediaIcon>
+            <SocialMediaIcon><Image src={TwitterIcon}/></SocialMediaIcon>
+            <SocialMediaIcon><Image src={YouTubeIcon}/></SocialMediaIcon>
+          </SocialMediaGrid>
+          <ButtonContainer><ContactUsButton>CONTACT US</ContactUsButton></ButtonContainer>
+          <LogoContainer><Logo>Present AI</Logo></LogoContainer>
+        </ThirdContainer>
+        <EndingCaption>&copy; {new Date().getFullYear()} Present AI Inc.</EndingCaption>
+      </MainContainer>
+    </MainBody>
   )
 }
+
+const MainBody = styled.div`
+display: flex;
+width: 100%;
+height: 16vw;
+background-color: #003366;
+justify-content: center;
+align-items: center;
+`
+const MainContainer = styled.div`
+display: flex;
+margin-bottom: auto;
+width: 95%;
+height: 90%;
+// background-color: purple;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+`
+const EndingCaption = styled.div`
+display: flex;
+font-size: 2vw;
+color: white;
+`
+
+const ThirdContainer = styled.div`
+display: flex;
+width: 95%;
+height: 80%;
+// background-color: blue;
+margin-top: auto;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+`
+const SocialMediaGrid = styled.div`
+display: flex;
+height: 90%;
+width: 30%;
+// background-color: yellow;
+margin: auto auto;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+`
+const SocialMediaIcon = styled.div`
+display: flex;
+margin: auto auto;
+img{
+  width: 5vw;
+  height: 5vw;
+  &:hover{
+    transform: scale(1.1);
+  }
+}
+`
+
+const ButtonContainer = styled.div`
+display: flex;
+height: 90%;
+width: 30%;
+// background-color: violet;
+margin: auto auto;
+justify-content: center;
+align-items: center;
+`
+const LogoContainer = styled.div`
+display: flex;
+height: 90%;
+width: 30%;
+// background-color: orange;
+margin: auto auto;
+justify-content: center;
+align-items: center;
+`
+const Logo = styled.div`
+margin: auto auto;
+color: white;
+font-family: "Archivo Black", sans-serif; 
+font-size: 4vw;
+`
+const ContactUsButton = styled.button`
+margin: auto auto;
+// background-color: red;
+background-color: white;
+// border-radius: 0.75vw;
+// border: 0.4vw solid white;
+border: none;
+padding: 0.75vw 1.5vw;
+color: #003366;
+font-size: 3vw;
+font-weight: 900;
+
+&:hover{
+  background-color: #003366;
+  color: white;
+}
+`
 
 export default Footer
--- present-ai-app/Components/MainPageSection2.js
+++ present-ai-app/Components/MainPageSection2.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import MaximizePotential from '../assets/MaximizePotential.png'
+import CommunicateEffectively from '../assets/CommunicateEffectively.png'
+import SpeakConfidently from '../assets/SpeakConfidently.png'
+import Image from 'next/image';
+import { keyframes } from 'styled-components';
 
 const MainPageSection2 = () => {
   return (
@@ -7,12 +12,12 @@ const MainPageSection2 = () => {
       <MainHeading>Our goal is to make you...</MainHeading>
       <MainContainer>
         <MainGrid>
-          <GridText>TEXT</GridText>
-          <GridIcon>ICON</GridIcon>
-          <GridText>TEXT</GridText>
-          <GridIcon>ICON</GridIcon>
-          <GridText>TEXT</GridText>
-          <GridIcon>ICON</GridIcon>
+          <GridItem><GridText>Communicate Effectively</GridText></GridItem>
+          <GridItem><GridIcon><Image src={SpeakConfidently} alt = "Speak Confidently" layout='intrinsic'/></GridIcon></GridItem>
+          <GridItem><GridText>Maximize Potential</GridText></GridItem>
+          <GridItem><GridIcon><Image src={CommunicateEffectively} alt = "Communicate Effectively" layout='intrinsic'/></GridIcon></GridItem>
+          <GridItem><GridText>Speak Confidently</GridText></GridItem>
+          <GridItem><GridIcon><Image src={MaximizePotential} alt = "Maximize Potential" layout='intrinsic'/></GridIcon></GridItem>
         </MainGrid>
       </MainContainer>
     </MainBody>
@@ -22,8 +27,8 @@ const MainPageSection2 = () => {
 const MainBody = styled.div`
 display: flex;
 width: 100%;
-height: 50vw;
-background-color: lightgreen;
+height: 60vw;
+// background-color: lightgreen;
 justify-content: center;
 align-items: center;
 flex-direction: column;
@@ -31,23 +36,25 @@ flex-direction: column;
 const MainContainer = styled.div`
 justify-content: center;
 align-items: center;
-background-color: yellow;
+// background-color: yellow;
 display: flex;
 width: 90%;
-height: 90%;
+height: 65%;
+margin-bottom: auto;
 `
 const MainHeading = styled.div`
+margin-top: auto;
 font-size: 6vw;
 display: flex;
 font-weight: 700;
 letter-spacing: 0.1vw;
-color: black;
-background-color: aqua;
+color: #003366;
+// background-color: aqua;
 
 `
 
 const MainGrid = styled.div`
-background-color: deeppink;
+// background-color: deeppink;
 width: 90%;
 height: 90%;
 display: grid;
@@ -56,23 +63,49 @@ grid-template-rows: repeat(2, 1fr);
 grid-column-gap: 1vw;
 grid-row-gap: 1vw;
 `
+const GridItem = styled.div`
+display: flex;
+justify-content: center;
+align-content: center;
+// background-color: purple;
+text-align: center;
+`
+
 const GridText = styled.div`
+width: 100%;
 margin: auto auto;
 display: flex; 
-width: 100%;
-height: 100%;
-background-color: lightgreen;
-font-size: 5vw;
-color: black;
+// background-color: lightgreen;
+font-size: 4vw;
+color: #003366;
 `
+
+const Pulse = keyframes`
+0% {transform: scale(1);}
+25% {transform: scale(1.05);}
+50%{transform: scale(1.1);}
+75% {transform: scale(1.05);}
+100%{transform: scale(1);}
+`
+
 const GridIcon = styled.div`
 margin: auto auto;
 display: flex; 
-width: 100%;
-height: 100%;
-background-color: lightblue;
-font-size: 5vw;
-color: black;
+width: 95%;
+height: 95%;
+// background-color: lightblue;
+// color: black;
+img{
+  margin: auto auto;
+  width: 60%;
+  heigh: 60%;
+  &:hover{
+    animation-name: ${Pulse};
+    animation-duration: 4s;
+    animation-iteration-count: infinite;
+    
+  }
+}
 `
 
 export default MainPageSection2
